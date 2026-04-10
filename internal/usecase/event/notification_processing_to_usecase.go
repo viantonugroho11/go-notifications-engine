ec48ce1be2ec74766e581b1626afa5e59bba697e
Muhammad Novianto Nugroho <novianto.nug@gmail.com>
2026-04-10 09:32:32 +0700

    feat: update usecase for sending to person

--- internal/usecase/event/notification_processing_to_usecase.go
+++ internal/usecase/event/notification_processing_to_usecase.go
@@ -2,16 +2,74 @@ package event
 
 import (
 	"context"
+	"errors"
+	"time"
+
+	"go-boilerplate-clean/internal/client/email"
+	"go-boilerplate-clean/internal/client/firebase"
+	"go-boilerplate-clean/internal/client/notification"
+	"go-boilerplate-clean/internal/entity/notificationlogs"
+	"go-boilerplate-clean/internal/entity/notifications"
 )
 
 type NotificationProcessingToUsecase interface {
-	Process(ctx context.Context, n Notification) error
+	Process(ctx context.Context, n notifications.NotificationEventUsecase) error
 }
 
 type notificationProcessingToUsecase struct {
-	notificationRepository notificationRepository
+	notificationClient notification.Client
+	emailClient        email.EmailClient
+	firebaseClient     firebase.FirebaseClient
 }
 
-func NewNotificationProcessingToUsecase(notificationRepository notificationRepository) NotificationProcessingToUsecase {
-	return &notificationProcessingToUsecase{notificationRepository: notificationRepository}
+func NewNotificationProcessingToUsecase(notificationClient notification.Client, emailClient email.EmailClient, firebaseClient firebase.FirebaseClient) NotificationProcessingToUsecase {
+	return &notificationProcessingToUsecase{notificationClient: notificationClient, emailClient: emailClient, firebaseClient: firebaseClient}
+}
+
+func (s *notificationProcessingToUsecase) Process(ctx context.Context, n notifications.NotificationEventUsecase) error {
+	var remark string
+	var status notificationlogs.State
+
+	switch n.Channel {
+	case notifications.ChannelEmail:
+		err := s.emailClient.Send(ctx, &email.Message{
+			To:      []string{n.NotificationLogs.SendTo},
+			Subject: n.NotificationLogs.RenderedSubject,
+			Body:    n.NotificationLogs.RenderedMessage,
+		})
+		if err != nil {
+			remark = err.Error()
+		}
+	case notifications.ChannelPush:
+		messageID, err := s.firebaseClient.Send(ctx, firebase.Message{
+			Token: n.NotificationLogs.SendTo,
+			Notification: &firebase.Notification{
+				Title: n.NotificationLogs.RenderedSubject,
+				Body:  n.NotificationLogs.RenderedMessage,
+			},
+		})
+		if err != nil {
+			remark = err.Error()
+		}
+		remark = messageID
+	default:
+		return errors.New("channel not supported")
+	}
+
+	status = notificationlogs.StateCompleted
+	if remark != "" {
+		status = notificationlogs.StateFailed
+	}
+
+	now := time.Now()
+	_, err := s.notificationClient.UpdateNotificationLog(ctx, notificationlogs.NotificationLog{
+		ID:           n.NotificationLogs.ID,
+		State:        status,
+		ErrorMessage: remark,
+		SentAt:       &now,
+	})
+	if err != nil {
+		return err
+	}
+	return nil
 }
